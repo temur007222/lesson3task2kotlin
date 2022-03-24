91ff47736463c79987c26928e6dd68bf9f5f527e
temur007222 <temurxaydarov1@gmail.com>
2022-03-24 17:01:41 +0500

    done

--- app/src/main/java/com/example/lesson3task2kotlin/MainActivity.kt
+++ app/src/main/java/com/example/lesson3task2kotlin/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.lesson3task2kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+    private fun initView(){
+        val i = findViewById<Button>(R.id.i)
+        i.setOnClickListener { secondActivity() }
+    }
+    private fun secondActivity(){
+        val second = Intent(this, SecondActivity::class.java)
+        startActivity(second)
     }
 }
---
+++ app/src/main/java/com/example/lesson3task2kotlin/SecondActivity.kt
@@ -0,0 +1,10 @@
+package com.example.lesson3task2kotlin
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_activity)
+    }
+}
